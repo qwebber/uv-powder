7ae9b1f6bbbfb1a85c72d037d054e4dde7356540
Quinn Webber <webber.quinn@gmail.com>
2020-06-08 10:25:40 -0230

    all data to model

--- scripts/5-p.zero.R
+++ scripts/5-p.zero.R
@@ -6,10 +6,13 @@ library(ggplot2)
 library(effects)
 
 p.zero <- fread("output/p.zero.csv")
+p.zero$inf <- p.zero$total.inf.F + p.zero$total.inf.M
 
 ## Social model
-F1 = lm(log(total.inf.F) ~ sex + act_hb + exp_hb + act_ym  + soc_ym + min.ta, data = p.zero)
-summary(F1)
+A1 = lm(log(inf) ~ sex + act_hb + exp_hb + act_ym  + soc_ym + min.ta, data = p.zero)
+summary(A1)
+
+
 M1 = lm(log(total.inf.M) ~ sex + act_hb + exp_hb + act_ym  + soc_ym + min.ta, data = p.zero)
 summary(M1)
 
