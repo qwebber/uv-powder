aac8b20cee46b16ddbf64be834ddde020f4611e9
Quinn Webber <webber.quinn@gmail.com>
2020-06-30 10:04:23 -0230

    code for FigS4

--- scripts/FigureS4.R
+++ scripts/FigureS4.R
@@ -17,94 +17,96 @@ theme1 <- theme(legend.position = 'none',
                 panel.background = element_blank(), 
                 panel.border = element_rect(colour = "black", fill=NA, size = 1))
 
+col = c("#1b9e77", "#1f78b4")
+
 png("figures/FigureS4.png", width = 7000, height = 3500, units = "px", res = 600)
-aa <- ggplot(dust2, aes(act_hb, exp_hb)) +
+aa <- ggplot(dust, aes(act_hb, exp_hb)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("A)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Exploration PC2" [H], ")" )) +
   xlab(expression("Activity PC1" [H], ")" )) + 
   theme1
-bb <- ggplot(dust2, aes(act_hb, act_ym)) +
+bb <- ggplot(dust, aes(act_hb, act_ym)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("B)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Activity PC1" [Y], ")" )) +
   xlab(expression("Activity PC1" [H], ")" )) + 
   theme1
-cc <- ggplot(dust2, aes(act_hb, soc_ym)) +
+cc <- ggplot(dust, aes(act_hb, soc_ym)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("C)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Sociability PC2" [Y], ")" )) +
   xlab(expression("Activity PC1" [H], ")" )) + 
   theme1
-dd <- ggplot(dust2, aes(exp_hb, act_ym)) +
+dd <- ggplot(dust, aes(exp_hb, act_ym)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("D)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Activity PC1" [Y], ")" )) +
   xlab(expression("Exploration PC2" [H], ")" )) + 
   theme1
-ee <- ggplot(dust2, aes(exp_hb, soc_ym)) +
+ee <- ggplot(dust, aes(exp_hb, soc_ym)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("E)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Sociability PC2" [Y], ")" )) +
   xlab(expression("Exploration PC2" [H], ")" )) + 
   theme1
-ff <- ggplot(dust2, aes(act_ym, soc_ym)) +
+ff <- ggplot(dust, aes(act_ym, soc_ym)) +
   geom_point(aes(color=sex)) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("F)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   ylab(expression("Sociability PC2" [Y], ")" )) +
   xlab(expression("Activity PC1" [Y], ")" )) + 
   theme1
-gg <- ggplot(dust2, aes(dawn.ta, act_hb)) +
+gg <- ggplot(dust, aes(dawn.ta, act_hb)) +
   geom_jitter(aes(color=sex), width = 1) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("G)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   labs(x = expression(paste("Temperature (",degree~C,")"))) +
   ylab(expression("Activity PC1" [H], ")" )) + 
   theme1
-hh <- ggplot(dust2, aes(dawn.ta, exp_hb)) +
+hh <- ggplot(dust, aes(dawn.ta, exp_hb)) +
   geom_jitter(aes(color=sex), width = 1) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("H)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   labs(x = expression(paste("Temperature (",degree~C,")"))) +
   ylab(expression("Exploration PC2" [H], ")" )) + 
   theme1
-ii <- ggplot(dust2, aes(dawn.ta, act_ym)) +
+ii <- ggplot(dust, aes(dawn.ta, act_ym)) +
   geom_jitter(aes(color=sex), width = 1) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("I)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   labs(x = expression(paste("Temperature (",degree~C,")"))) +
   ylab(expression("Activity PC1" [Y], ")" )) + 
   theme1
-jj <- ggplot(dust2, aes(dawn.ta, soc_ym)) +
+jj <- ggplot(dust, aes(dawn.ta, soc_ym)) +
   geom_jitter(aes(color=sex), width = 1) +
   geom_smooth(method = "lm", se = F,
-              color = "black", lty = 2) +
+              color = "black") +
   ggtitle("J)") +
-  scale_color_brewer(palette = "Dark2") +
+  scale_color_manual(values = col) +
   labs(x = expression(paste("Temperature (",degree~C,")"))) +
   ylab(expression("Sociability PC2" [Y], ")" )) + 
   theme1
