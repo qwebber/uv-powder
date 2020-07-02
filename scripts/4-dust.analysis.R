4e417bf99d9297f5b8cf94a67969b4d6b3765525
Quinn Webber <webber.quinn@gmail.com>
2020-07-02 09:59:04 -0230

    Update 4-dust.analysis.R

--- scripts/4-dust.analysis.R
+++ scripts/4-dust.analysis.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(merTools)
 
 dust <- fread("output/all.bats.csv")
+p.zero <- fread("output/p.zero.csv")
 
 ##sex ratio
 aa <- dust[sex == "F"][, .N , by = "Trial"]
@@ -37,3 +38,35 @@ global = lmer(log(total.inf) ~ sex + act_hb + exp_hb +
           data = dust)
 summary(global)
 
+## all data in same model
+
+p.zero2 <- p.zero[,c("Trial","act_hb", "exp_hb", "act_ym", "soc_ym", "sex")]
+colnames(p.zero2) <- c("Trial", "act_hb_pz", "exp_hb_pz", 
+                       "act_ym_pz", "soc_ym_pz" ,"sex_pz")
+dust2 <- dust[,c("Trial","total.inf" ,"act_hb", "exp_hb", 
+                 "act_ym", "soc_ym", "sex", "dawn.ta" ,"ID")]
+
+df <- merge(dust2, p.zero2, by = "Trial")
+df$sex_combo <- paste(dust2$sex, p.zero2$sex_pz, sep = "_")
+
+
+global2 <- lmer(log(total.inf) ~ sex_combo + act_hb + sex*exp_hb + 
+  act_ym + sex*soc_ym + sex_pz*act_hb_pz + sex_pz*exp_hb_pz + 
+  sex_pz*act_ym_pz + sex_pz*soc_ym_pz + dawn.ta + (1|Trial),  data = df)
+
+summary(global2)
+
+ggplot(df, aes(soc_ym_pz, log(total.inf), color = sex)) +
+  geom_jitter() +
+  geom_smooth(method = "lm", se = F)
+
+ggplot(df, aes(sex_combo, log(total.inf), fill = sex_pz)) +
+  geom_boxplot(notch = TRUE,
+               outlier.color = NA,
+               lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = sex_pz), shape = 16,
+              position = position_jitterdodge(0.3),
+              size = 2,
+              alpha = 0.6) 
+  theme1
