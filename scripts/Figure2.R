a1df60260bba0eb7bb2b145b69508b535d96375c
Quinn Webber <webber.quinn@gmail.com>
2020-07-02 12:07:29 -0230

    add two models to same figure

--- scripts/Figure2.R
+++ scripts/Figure2.R
@@ -8,32 +8,42 @@ library(dotwhisker)
 
 dust <- fread("output/all.bats.csv")
 
-
+## Acquisition model
 global = lmer(log(total.inf) ~ sex*act_hb + sex*exp_hb + 
                 sex*act_ym + sex*soc_ym  + dawn.ta + (1|Trial), 
               data = dust)
-summary(global)
-
 coef <- tidy(global, conf.int = TRUE)
 
+## Transmission model
+pz <- lm(log(total.inf) ~ sex + act_hb + exp_hb + 
+           act_ym + soc_ym  + dawn.ta, 
+         data = p.zero)
+pz2 <- tidy(pz)
+
+dwplot(list(pz, global))
+
+
 png("figures/Figure2.png", width = 4000, height = 2500, units = "px", res = 600)
-dwplot(coef[1:11,], 
-       vline = geom_vline(xintercept = 0, colour = "black", linetype = 2)) %>% # plot line at zero _behind_ coefs
-  relabel_predictors(c(`sexM:exp_hb` = "Sex : Exploration (H)", 
-                       sexM = "Sex",   
+dwplot(list(pz, global),
+             vline = geom_vline(xintercept = 0, colour = "black", linetype = 2)) %>% # plot line at zero _behind_ coefs
+  relabel_predictors(c(sexM = "Sex", 
                        dawn.ta = "Temperature",
-                       act_ym = "Activity (Y)", 
                        act_hb = "Activity (H)", 
+                       exp_hb = "Exploration (H)",
+                       act_ym = "Activity (Y)", 
+                       soc_ym = "Sociability (Y)",
                        `sexM:act_hb` = "Sex : Activity (H)", 
-                       soc_ym = "Sociability (Y)", 
+                       `sexM:exp_hb` = "Sex : Exploration (H)", 
                        `sexM:act_ym` = "Sex : Activity (Y)", 
-                       `sexM:soc_ym` = "Sex : Sociability (Y)", 
-                       exp_hb = "Exploration (H)")) +
-  scale_colour_manual(values = "grey60") +
+                       `sexM:soc_ym` = "Sex : Sociability (Y)",
+                       `sd_(Intercept).Trial` = "Variance", 
+                       `sd_Observation.Residual` = "Residual Variance"))+
+  scale_colour_manual(values = c("#66c2a5", "#fc8d62")) +
   xlab("Coefficient Estimate") + 
   ylab("") +
   geom_vline(xintercept = 0, colour = "black", linetype = 2) +
-  theme(legend.position = 'none',
+  theme(legend.position = c(0.1,0.1),
+        legend.title = element_blank(),
         legend.key = element_blank(),
         axis.title = element_text(size = 14, color = 'black'),
         axis.text = element_text(size = 12, color = 'black'),
@@ -41,5 +51,3 @@ dwplot(coef[1:11,],
         panel.background = element_blank(), 
         panel.border = element_rect(colour = "black", fill=NA, size = 1))
 dev.off()
-
-
